d018fb70b79656db44a6cb62174b5193fb8f56f3
MrHeer <hlm52pk@163.com>
2020-12-05 12:14:58 +0800

    feat: add time text widget

---
+++ lib/time_text.dart
@@ -0,0 +1,200 @@
+library time_text;
+
+import 'dart:async';
+import 'dart:ui' as ui show TextHeightBehavior;
+
+import 'package:flutter/widgets.dart';
+import 'package:intl/intl.dart' show DateFormat;
+
+typedef String Formatter(DateTime dateTime);
+
+/// A run of text with a single style to show current time.
+///
+/// The [Text] widget displays a string of text with single style. The string
+/// might break across multiple lines or might all be displayed on the same line
+/// depending on the layout constraints.
+///
+/// The [style] argument is optional. When omitted, the text will use the style
+/// from the closest enclosing [DefaultTextStyle]. If the given style's
+/// [TextStyle.inherit] property is true (the default), the given style will
+/// be merged with the closest enclosing [DefaultTextStyle]. This merging
+/// behavior is useful, for example, to make the text bold while using the
+/// default font family and size.
+class TimeText extends StatefulWidget {
+  /// Creates a time text widget.
+  ///
+  /// If the [style] argument is null, the text will use the style from the
+  /// closest enclosing [DefaultTextStyle].
+  ///
+  /// The [overflow] property's behavior is affected by the [softWrap] argument.
+  /// If the [softWrap] is true or null, the glyph causing overflow, and those that follow,
+  /// will not be rendered. Otherwise, it will be shown with the given overflow option
+  TimeText(
+      {Key key,
+        formatter,
+        this.duration = const Duration(seconds: 1),
+        this.style,
+        this.strutStyle,
+        this.textAlign,
+        this.textDirection,
+        this.locale,
+        this.softWrap,
+        this.overflow,
+        this.textScaleFactor,
+        this.maxLines,
+        this.semanticsLabel,
+        this.textWidthBasis,
+        this.textHeightBehavior})
+      : this.formatter = formatter ?? DateFormat.Hm().format,
+        super(key: key);
+
+  // time formatter, default [DateFormat().Hm().format]
+  final Formatter formatter;
+
+  // update duration, default [Duration(seconds: 1)]
+  final Duration duration;
+
+  /// If non-null, the style to use for this text.
+  ///
+  /// If the style's "inherit" property is true, the style will be merged with
+  /// the closest enclosing [DefaultTextStyle]. Otherwise, the style will
+  /// replace the closest enclosing [DefaultTextStyle].
+  final TextStyle style;
+
+  /// {@macro flutter.painting.textPainter.strutStyle}
+  final StrutStyle strutStyle;
+
+  /// How the text should be aligned horizontally.
+  final TextAlign textAlign;
+
+  /// The directionality of the text.
+  ///
+  /// This decides how [textAlign] values like [TextAlign.start] and
+  /// [TextAlign.end] are interpreted.
+  ///
+  /// This is also used to disambiguate how to render bidirectional text. For
+  /// example, if the [data] is an English phrase followed by a Hebrew phrase,
+  /// in a [TextDirection.ltr] context the English phrase will be on the left
+  /// and the Hebrew phrase to its right, while in a [TextDirection.rtl]
+  /// context, the English phrase will be on the right and the Hebrew phrase on
+  /// its left.
+  ///
+  /// Defaults to the ambient [Directionality], if any.
+  final TextDirection textDirection;
+
+  /// Used to select a font when the same Unicode character can
+  /// be rendered differently, depending on the locale.
+  ///
+  /// It's rarely necessary to set this property. By default its value
+  /// is inherited from the enclosing app with `Localizations.localeOf(context)`.
+  ///
+  /// See [RenderParagraph.locale] for more information.
+  final Locale locale;
+
+  /// Whether the text should break at soft line breaks.
+  ///
+  /// If false, the glyphs in the text will be positioned as if there was unlimited horizontal space.
+  final bool softWrap;
+
+  /// How visual overflow should be handled.
+  ///
+  /// Defaults to retrieving the value from the nearest [DefaultTextStyle] ancestor.
+  final TextOverflow overflow;
+
+  /// The number of font pixels for each logical pixel.
+  ///
+  /// For example, if the text scale factor is 1.5, text will be 50% larger than
+  /// the specified font size.
+  ///
+  /// The value given to the constructor as textScaleFactor. If null, will
+  /// use the [MediaQueryData.textScaleFactor] obtained from the ambient
+  /// [MediaQuery], or 1.0 if there is no [MediaQuery] in scope.
+  final double textScaleFactor;
+
+  /// An optional maximum number of lines for the text to span, wrapping if necessary.
+  /// If the text exceeds the given number of lines, it will be truncated according
+  /// to [overflow].
+  ///
+  /// If this is 1, text will not wrap. Otherwise, text will be wrapped at the
+  /// edge of the box.
+  ///
+  /// If this is null, but there is an ambient [DefaultTextStyle] that specifies
+  /// an explicit number for its [DefaultTextStyle.maxLines], then the
+  /// [DefaultTextStyle] value will take precedence. You can use a [RichText]
+  /// widget directly to entirely override the [DefaultTextStyle].
+  final int maxLines;
+
+  /// An alternative semantics label for this text.
+  ///
+  /// If present, the semantics of this widget will contain this value instead
+  /// of the actual text. This will overwrite any of the semantics labels applied
+  /// directly to the [TextSpan]s.
+  ///
+  /// This is useful for replacing abbreviations or shorthands with the full
+  /// text value:
+  ///
+  /// ```dart
+  /// Text(r'$$', semanticsLabel: 'Double dollars')
+  /// ```
+  final String semanticsLabel;
+
+  /// {@macro flutter.painting.textPainter.textWidthBasis}
+  final TextWidthBasis textWidthBasis;
+
+  /// {@macro flutter.dart:ui.textHeightBehavior}
+  final ui.TextHeightBehavior textHeightBehavior;
+
+  @override
+  State<StatefulWidget> createState() => _TimeTextState();
+}
+
+class _TimeTextState extends State<TimeText> {
+  String _timeString;
+  Timer _timer;
+
+  void _getTime() {
+    final DateTime now = DateTime.now();
+    final String formattedDateTime = _formatDateTime(now);
+    setState(() {
+      _timeString = formattedDateTime;
+    });
+  }
+
+  String _formatDateTime(DateTime dateTime) {
+    return this.widget.formatter(dateTime);
+  }
+
+  @override
+  void initState() {
+    _timeString = _formatDateTime(DateTime.now());
+    _timer = Timer.periodic(Duration(seconds: 1), (Timer t) => _getTime());
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    if(_timer != null) {
+      _timer.cancel();
+    }
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      _timeString,
+      style: this.widget.style,
+      strutStyle: this.widget.strutStyle,
+      textAlign: this.widget.textAlign,
+      textDirection: this.widget.textDirection,
+      locale: this.widget.locale,
+      softWrap: this.widget.softWrap,
+      overflow: this.widget.overflow,
+      textScaleFactor: this.widget.textScaleFactor,
+      maxLines: this.widget.maxLines,
+      semanticsLabel: this.widget.semanticsLabel,
+      textWidthBasis: this.widget.textWidthBasis,
+      textHeightBehavior: this.widget.textHeightBehavior,
+    );
+  }
+}
---
+++ test/time_text_test.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:intl/intl.dart' show DateFormat;
+
+import 'package:time_text/time_text.dart';
+
+void main() {
+  testWidgets('TimeText has a current time', (WidgetTester tester) async {
+    await tester.pumpWidget(TimeText(textDirection: TextDirection.ltr));
+    final currentTime = DateFormat.Hm().format(DateTime.now());
+    final timeTextFinder = find.text(currentTime);
+    expect(timeTextFinder, findsOneWidget);
+  });
+
+  testWidgets('TimeText has a current time with a formatter', (WidgetTester tester) async {
+    await tester.pumpWidget(TimeText(formatter: DateFormat.Hms().format, textDirection: TextDirection.ltr));
+    final currentTime = DateFormat.Hms().format(DateTime.now());
+    final timeTextFinder = find.text(currentTime);
+    expect(timeTextFinder, findsOneWidget);
+  });
+}
