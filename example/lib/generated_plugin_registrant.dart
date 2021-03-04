2f61d535c15a86cc66a9a9d1d5933225bf73149e
MrHeer <hlm52pk@163.com>
2021-03-04 11:53:08 +0800

    fix: nullable

--- example/lib/generated_plugin_registrant.dart
+++ example/lib/generated_plugin_registrant.dart
@@ -4,14 +4,12 @@
 
 // ignore_for_file: lines_longer_than_80_chars
 
-import 'package:import_js_library/import_js_library.dart';
 import 'package:wakelock_web/wakelock_web.dart';
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 // ignore: public_member_api_docs
 void registerPlugins(Registrar registrar) {
-  ImportJsLibrary.registerWith(registrar);
   WakelockWeb.registerWith(registrar);
   registrar.registerMessageHandler();
 }
